4a1d4396d6674866fb2468ce4f5ae275c2b9ef6d
堂福大成 <>
2018-07-31 13:18:53 +0900

    Add name column for sign_up,account_update devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  #devise関連の時だけアクションが作動する。deviseのヘルパーメソッドの１つ
+
+  protected
+
+  def configure_permitted_parameters
+    # sign_upのときに、nameも許可する
+      devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+    #account_updateのときに、nameも許可する
+      devise_parameter_sanitizer.permit(:account_update, keys:[:name])
+  end
 end

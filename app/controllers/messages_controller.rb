9e1c90b8e2425808d28cd0e8f42734fbc0ae9009
堂福大成 <>
2018-08-03 21:22:39 +0900

    Make send message func, views

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,24 @@
 class MessagesController < ApplicationController
+  def index
+    @message = Message.new
+  end
+
+  def create
+    @group = Group.find(params[:group_id]) #この:group_idは、URIからとってきたので、文字列
+    @message = @group.messages.new(message_params)
+    if @message.save
+      flash[:notice] = "メッセージを送信しました。"
+      redirect_to group_messages_path
+    else
+      flash[:alert] = "メッセージを入力してください。"
+      render action: :index
+    end
+  end
+
+
+  private
+
+  def message_params
+    params.require(:message).permit(:text, :image).merge(user_id: current_user.id)
+  end
 end

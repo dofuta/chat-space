ab461fab6f949f99e6d0ff9c5cebc6ae40de7f8a
堂福大成 <>
2018-09-17 16:53:29 +0900

    Fix a typo

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,8 +7,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+    aws_access_key_id: Rails.application.secrets.aws_s3_access_key_id,
+    aws_secret_access_key: Rails.application.secrets.aws_s3_secret_access_key,
     region: 'us-east-1'
   }
 
